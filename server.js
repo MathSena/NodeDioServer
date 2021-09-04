9ae8cda38e3839bc70ed8a30f2d1e3b3a7b0ee50
MathSena <MathSena07@hotmail.com>
2021-09-03 21:23:18 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const host = 'http://localhost';
+const port = 3000;
+const stats = require('./pcRamUsage.js'); // Importando outro módulo
+
+http.createServer((req, res) =>{
+    let url = req.url;
+
+    if(url === '/stats'){
+        res.end(JSON.stringify(stats, null, 2));
+    }else{
+        res.end('<h1>Welcome to the family</h1>')
+    }
+
+    
+}).listen(port, ()=> console.log((`Server is running ${host}:${port}`))); // Criando servidor
